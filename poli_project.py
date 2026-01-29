487fcd4fe2750f3573805cf827dd44acebb501a3
Rajasekhar423 <rajasekharkaranam0@gmail.com>
2026-01-29 12:44:32 +0530

    poli_project is file added

---
+++ poli_project.py
@@ -0,0 +1,26 @@
+
+
+def reverse(num):
+    rev = 0
+    while num > 0:
+        rev = rev * 10 + num % 10
+        num //= 10
+    return rev  
+
+def ispalindrome(num):
+    return num == reverse(num)
+
+print(reverse(123))        
+print(ispalindrome(123))  
+
+print(reverse(121))        
+print(ispalindrome(121))        
+
+
+def getpalindrome(start,end):
+    res=""
+    for i in range(1,end+1):
+        if ispalindrome(i):
+            res=res+str(i)+"."
+    return res
+print(getpalindrome(1,10000))
